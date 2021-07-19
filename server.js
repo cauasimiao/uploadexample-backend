93004403d3e20ef159286048ca38bae02ccbaa81
cauasimiao <caua.simiao@outlook.com>
2021-07-19 14:52:08 -0300

    servidor

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 global.__basedir = __dirname;
-
+/*
 var corsOptions = {
   origin: "http://localhost:8081"
 };
-
+*/
 app.use(cors(corsOptions));
 
 const initRoutes = require("./src/routes");
